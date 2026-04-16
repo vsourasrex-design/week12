9484411ffb70e42933759d443ad80f2a5038e4c7
vsourasrex-design <vsourasrex@gmail.com>
2026-04-15 21:27:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const box = document.querySelector(".box");
   const colorButton = document.getElementById("changeColor");
   const toggleButton = document.getElementById("toggleImage");
- 
- 
+
   colorButton.addEventListener("click", () => {
-    bx.style.backgroundColor = "blue"; 
+    box.style.backgroundColor = "blue"; 
   });
- 
- 
- 
+
   let toggled = false;
+
   toggleButton.addEventListener("click", () => {
     if (toggled) {
       gallery.src = "./assets/image1.jpg";
       toggled = false;
     } else {
-      gallery.src = "./assets/image-does-not-exist.jpg"; 
+      gallery.src = "./assets/image1.jpg"; 
       toggled = true;
     }
   });
- 
- 
- 
-  console.log("Page loaded!"
+
+  console.log("Page loaded!"); 
 });
